ed62575f0674a34c7341c356052ada042ea7a052
wangyaL <906072654@qq.com>
2015-08-07 16:21:51 +0800

    init

---
+++ src/com/wangyal/memory/Fragment2.java
@@ -0,0 +1,20 @@
+package com.wangyal.memory;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 
+ * @author wangyaL 2015年8月7日 下午3:50:31
+ */
+public class Fragment2 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg2, container,false);
+        return view;
+    }
+}
